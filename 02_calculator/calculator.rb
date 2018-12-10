51b0da3e930c0152793b04bf1afe2a3ff006e10b
Illges <brown1ei.eb@gmail.com>
2018-12-10 13:19:57 -0500

    Finished hello, temperature and calculator exercises.

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,38 @@
 #write your code here
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(array)
+  sum = 0
+  array.each {|a| sum+=a}
+  sum
+end
+
+def multiply(*args)
+  product = 1
+  args.each {|a| product*=a}
+  product
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial (a)
+  if a == 0
+    1
+  else
+    fact = 1
+    i = 1
+    while i < (a + 1)
+      fact *= i
+      i+=1
+    end
+    fact
+  end
+end
